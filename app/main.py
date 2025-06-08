56ec0a8d6c581dc7fcb48fdfcbe7c4343479e741
MK <m.kanamaru.it@gmail.com>
2025-06-08 16:51:56 -0400

    setup FastAPI app

---
+++ app/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Change later for frontend URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello from Job Tracker API"}
