767a0e0bc940c88f033fe33752add1d0eb74286c
MK <m.kanamaru.it@gmail.com>
2025-06-14 16:28:03 -0400

    fix workflow file

--- app/core/security.py
+++ app/core/security.py
@@ -8,8 +8,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-SECRET_KEY = os.getenv("SECRET_KEY")
-ALGORITHM = "HS256"
+SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret-key")
+ALGORITHM = os.getenv("ALGORITHM", "HS256")
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
